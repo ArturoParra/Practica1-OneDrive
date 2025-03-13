4177efdb83782fe79cda26bbb27484aba53439be
ArturoParra <aparrar1900@alumno.ipn.mx>
2025-03-12 19:47:33 -0600

    Archivos base del proyecto

---
+++ Practica1-OneDrive/src/main/java/com/mycompany/practica1/onedrive/Practica1OneDrive.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica1.onedrive;
+
+/**
+ *
+ * @author Artur
+ */
+public class Practica1OneDrive {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
