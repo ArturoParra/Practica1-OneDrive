09ab537ee4896897d0975918d92d31aade076127
Cristhian <cristhanatz@gmail.com>
2025-03-14 07:41:43 -0600

    xd

--- Practica1-OneDrive/src/main/java/com/mycompany/practica1/onedrive/Practica1OneDrive.java
+++ Practica1-OneDrive/src/main/java/com/mycompany/practica1/onedrive/Practica1OneDrive.java
@@ -9,9 +9,8 @@ package com.mycompany.practica1.onedrive;
  * @author Artur
  */
 public class Practica1OneDrive {
-
     public static void main(String[] args) {
+        System.out.println("Odio netbeans xd");
         System.out.println("Hello World!");
-        System.out.println("La vida a veces vale la pena");
     }
 }
