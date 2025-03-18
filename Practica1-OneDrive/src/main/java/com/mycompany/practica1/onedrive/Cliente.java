2fd518cb175fc5f5c7df91d1a7fc0296aaa79110
ArturoParra <aparrar1900@alumno.ipn.mx>
2025-03-18 09:14:21 -0600

    Implementación de menú, prompt de terminal, comandos funcionales, listar archivos locales y del servidor

--- Practica1-OneDrive/src/main/java/com/mycompany/practica1/onedrive/Cliente.java
+++ Practica1-OneDrive/src/main/java/com/mycompany/practica1/onedrive/Cliente.java
@@ -2,6 +2,7 @@ package com.mycompany.practica1.onedrive;
 
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class Cliente {
     public static void main(String[] args) {
@@ -14,14 +15,96 @@ public class Cliente {
             PrintWriter outControl = new PrintWriter(socketControl.getOutputStream(), true);
             BufferedReader inDatos = new BufferedReader(new InputStreamReader(socketDatos.getInputStream()));
             PrintWriter outDatos = new PrintWriter(socketDatos.getOutputStream(), true);
-
-            outControl.println("LIST");
-            String respuesta;
-            while ((respuesta = inDatos.readLine()) != null) {
-                System.out.println(respuesta);
+            if (socketControl.isConnected() && !socketControl.isClosed()) {
+                System.out.println("Conexión exitosa.");
+            } else {
+                System.out.println("Conexión fallida.");
             }
+
+            Scanner in = new Scanner(System.in);
+
+            String comando;
+            System.out.println("Bienvenido, Use el comando help para ver los comandos disponibles y el comando exit para salir de la aplicación.");
+            while(true){
+                System.out.print(">> ");
+                comando = in.nextLine();
+
+                if(!comando.equals("exit")){ //Verificar si el comando es exit
+                    switch(comando){ //Verificar si el comando es del cliente o del servidor
+                        // Comandos del cliente
+                        case "help":
+                            imprimirComandos();
+                            break;
+                        case "lsc":
+                            listarArchivos();
+                            break;
+                        default: //Comandos del servidor
+                            outControl.println(comando);
+                            String respuesta = inControl.readLine();
+                            if(respuesta != null){ //Respuesta del socket de control
+                                System.out.println(respuesta);
+                            }
+                            if (comando.equals("lss") || comando.equals("dwld") || comando.equals("upld")) { //Respuesta del socket de datos
+                                String respuesta2;
+                                while ((respuesta2 = inDatos.readLine()) != null) {
+                                    if (respuesta2.equals("END")) { //Verifica si ya son todos los datos para que el socket no se bloquee
+                                        break;
+                                    }
+                                    System.out.println(respuesta2);
+                                }
+                            }
+                            break;
+                    }
+                }else{ // Caso en el que el comando sea exit
+                    System.out.println("Saliendo de la aplicación..., Hasta luego.");
+                    break;
+                }
+
+            }
+
+            socketControl.close();
+            socketDatos.close();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
+
+    //Función que imprime el menú de comandos
+    private static void imprimirComandos(){
+        System.out.println("Comandos disponibles:");
+        System.out.println("lsc: Listar archivos del cliente");
+        System.out.println("lss: Listar archivos del servidor");
+        System.out.println("pwdc: Mostrar directorio actual del cliente");
+        System.out.println("pwds: Mostrar directorio actual del servidor");
+        System.out.println("cdc: Cambiar directorio en el cliente");
+        System.out.println("cds: Cambiar directorio en el servidor");
+        System.out.println("dwld: Descargar archivo del servidor");
+        System.out.println("upld: Subir archivo al servidor");
+        System.out.println("mkfiles: Crear archivo en el servidor");
+        System.out.println("mkfilec: Crear archivo en el cliente");
+        System.out.println("rmfiles: Eliminar archivo en el servidor");
+        System.out.println("rmfilec: Eliminar archivo en el cliente");
+        System.out.println("mkdirs: Crear directorio en el servidor");
+        System.out.println("mkdirc: Crear directorio en el cliente");
+        System.out.println("rmdirs: Eliminar directorio en el servidor");
+        System.out.println("rmdirc: Eliminar directorio en el cliente");
+        System.out.println("exit: Salir de la aplicación");
+    }
+
+    private static void listarArchivos() {
+        File directorio = new File("./dataclient");
+        File[] elementos = directorio.listFiles();
+        if (elementos != null) {
+            for (File elemento : elementos) {
+                if (elemento.isDirectory()) {
+                    System.out.println("Directorio: " + elemento.getName());
+                } else {
+                    System.out.println("Archivo: " + elemento.getName());
+                }
+            }
+        }else{
+            System.out.println("El directorio está vacío.");
+        }
+    }
+
 }
